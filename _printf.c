f8152af57bfd65063b09b35c64804c56932fb9d4
mube99 <mubarekfaris9@gmail.comfibjtrcdgit status     git config --global user.email mubarekfaris9@gmail.com>
2022-05-17 12:29:12 -0700

    creation of header file and printf file

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - formatted output conversion and print data.
+ * @format: input string
+ *
+ * Return: numbers of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	return (0);
+}
